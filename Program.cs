bea1bc4ccde7e5eef6dfa69b3c549c239083a1aa
Gøran Smistad <goran@smistad.org>
2020-09-16 13:25:42 +0200

    first version

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace YellowPages
 {
@@ -6,7 +9,54 @@ namespace YellowPages
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Contact> contacts = new List<Contact>();
+            contacts.Add(new Contact("Sean", "Skinner"));
+            contacts.Add(new Contact("Samantha", "Carter"));
+            contacts.Add(new Contact("Daniel", "Jackson"));
+            contacts.Add(new Contact("Sean", "Connery"));
+            contacts.Add(new Contact("Walter", "Skinner"));
+
+            for(int i = 0; i < contacts.Count; i++)
+            {
+                Console.WriteLine(contacts.ElementAt(i).toString());
+            }
+            Console.WriteLine("Type a name to search for:");
+            string search = "";
+            do
+            {
+                search = Console.ReadLine();
+                Console.WriteLine($"Showing results matching '{search}':");
+                for(int i = 0; i < contacts.Count; i++)
+                {
+                    if (contacts.ElementAt(i).search(search))
+                    {
+                        Console.WriteLine(contacts.ElementAt(i).toString());
+                    }
+                }
+            } while (search != "ragequit");
+        }
+    }
+
+    class Contact
+    {
+        string firstName, lastName;
+
+        public Contact(string a, string b)
+        {
+            firstName = a;
+            lastName = b;
+        }
+        public bool search(string searchString)
+        {
+            string fullName = firstName + " " + lastName;
+            string lowerSearch = searchString.ToLower();
+            
+            return firstName.ToLower().Contains(lowerSearch) || lastName.ToLower().Contains(lowerSearch) || fullName.ToLower().Contains(lowerSearch);
+        }
+
+        public string toString()
+        {
+            return firstName + " " + lastName;
         }
     }
 }
