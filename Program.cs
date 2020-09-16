6d854be2d8ca4642d623c84e204a9ec9968d2b72
Gøran Smistad <goran@smistad.org>
2020-09-16 11:08:55 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace YellowPages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
