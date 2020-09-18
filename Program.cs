c975a10143a54c56722f7bcb528a8453967984a3
Gøran Smistad <goran@smistad.org>
2020-09-18 11:05:38 +0200

    renamed Contact class and moved to separate file

--- Program.cs
+++ Program.cs
@@ -9,56 +9,51 @@ namespace YellowPages
     {
         static void Main(string[] args)
         {
-            List<Contact> contacts = new List<Contact>();
-            contacts.Add(new Contact("Sean", "Skinner"));
-            contacts.Add(new Contact("Samantha", "Carter"));
-            contacts.Add(new Contact("Daniel", "Jackson"));
-            contacts.Add(new Contact("Sean", "Connery"));
-            contacts.Add(new Contact("Walter", "Skinner"));
+            List<Person> contacts = new List<Person>();
+            contacts.Add(new Person("Sean", "Skinner"));
+            contacts.Add(new Person("Samantha", "Carter", 12345678));
+            contacts.Add(new Person("Daniel", "Jackson"));
+            contacts.Add(new Person("Sean", "Connery"));
+            contacts.Add(new Person("Walter", "Skinner", 87654321));
 
-            contacts.ForEach(delegate(Contact contact)
+            contacts.ForEach(delegate(Person p)
             {
-                Console.WriteLine(contact.Name);
+                Console.WriteLine(p.Details);
             });
 
            
-            Console.WriteLine("Type a name to search for:");
+            Console.WriteLine("Type a name or a number to search for:");
             string search = "";
             do
             {
                 search = Console.ReadLine();
                 Console.WriteLine($"Showing results matching '{search}':");
-                for(int i = 0; i < contacts.Count; i++)
+                try
                 {
-                    if (contacts.ElementAt(i).search(search))
+                    int phone = int.Parse(search);
+                    for(int i = 0; i < contacts.Count; i++)
                     {
-                        Console.WriteLine(contacts.ElementAt(i).Name);
+                        if (contacts.ElementAt(i).search(phone))
+                        {
+                            Console.WriteLine(contacts.ElementAt(i).Details);
+                        }
                     }
+                } 
+                catch
+                {
+                    for(int i = 0; i < contacts.Count; i++)
+                        {
+                            if (contacts.ElementAt(i).search(search))
+                            {
+                                Console.WriteLine(contacts.ElementAt(i).Details);
+                            }
+                        }
                 }
+
+
+
             } while (search != "ragequit");
         }
-    }
 
-    class Contact
-    {
-        private string firstName, lastName;
-        public string Name { get => firstName + " " + lastName; }
-
- 
-
-        public Contact(string a, string b)
-        {
-            firstName = a;
-            lastName = b;
-        }
-        public bool search(string searchString)
-        {
-            return caseInsensitiveMatch(firstName, searchString) || caseInsensitiveMatch(lastName, searchString) || caseInsensitiveMatch(Name, searchString);
-        }
-
-        private bool caseInsensitiveMatch(string text, string searchString)
-        {
-            return text.ToLower().Contains(searchString.ToLower());
-        }
     }
 }
